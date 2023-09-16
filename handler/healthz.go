6d3e2721ccf8b435aa95c220f0cca116709a3703
norm02 <richa0920@gmail.com>
2023-09-17 00:25:22 +0900

    healthz.goでhandler定義>router.goで登録>main.goで起動

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,13 +1,16 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
 // A HealthzHandler implements health check endpoint.
-type HealthzHandler struct{}
+type HealthzHandler struct{
+}
 
 // NewHealthzHandler returns HealthzHandler based http.Handler.
 func NewHealthzHandler() *HealthzHandler {
@@ -16,5 +19,16 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	resp := &model.HealthzResponse{
+	Message : "OK",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Println(err)
+	}
+	
+	
+
 }
