ccd78926cc8271f64c1559ef0f7fe5c54a34d518
norm02 <richa0920@gmail.com>
2023-10-23 22:55:06 +0900

    Add delete todo in service todo

--- service/todo.go
+++ service/todo.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
@@ -135,6 +137,32 @@ func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, descrip
 // DeleteTODO deletes TODOs on DB by ids.
 func (s *TODOService) DeleteTODO(ctx context.Context, ids []int64) error {
 	const deleteFmt = `DELETE FROM todos WHERE id IN (?%s)`
+	if len(ids) == 0 {
+		return nil
+	}
+	placeholders := strings.Repeat(",?", len(ids)-1)
+	query := fmt.Sprintf(deleteFmt, placeholders)
+
+	stmt, err := s.db.PrepareContext(ctx, query)
+	if err != nil {
+		return nil
+	}
+	defer stmt.Close()
+	args := make([]interface{}, len(ids))
+	for i, id := range ids {
+		args[i] = id
+	}
+	result, err := s.db.ExecContext(ctx,query,args...)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows==0{
+		return &model.ErrNotFound{}
+	}
 
 	return nil
 }
