859215c1772b0a58b670c368a302a4c22c7e66cb
norm02 <richa0920@gmail.com>
2023-12-27 20:37:15 +0900

    Add graceful shutdown & fix indent

--- handler/router/router.go
+++ handler/router/router.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/TechBowl-japan/go-stations/handler"
 	"github.com/TechBowl-japan/go-stations/handler/middleware"
@@ -21,36 +22,42 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux.HandleFunc("/todos", todoHandler.ServeHTTP)
 
 	/*
-	//"/do-panic"にアクセスすると、panicするmutex
-	mux.HandleFunc("/do-panic",http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		panic("surely panic")
-	}))
+		//"/do-panic"にアクセスすると、panicするmutex
+		mux.HandleFunc("/do-panic",http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			panic("surely panic")
+		}))
 	*/
 
 	//"/do-panic"にアクセスしても、recoverしてpanicしないmutex
-	mux.Handle("/do-panic",middleware.Recover(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	mux.Handle("/do-panic", middleware.Recover(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		panic("recover panic")
 	})))
 
-	mux.Handle("/os",middleware.StoreOS(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		os,err := middleware.CtxOS(r.Context())
-		if err!= nil{
+	mux.Handle("/os", middleware.StoreOS(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		os, err := middleware.CtxOS(r.Context())
+		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 		fmt.Println(os)
 	})))
 
-	mux.Handle("/accesslog",middleware.StoreOS(middleware.AccessLogger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	mux.Handle("/accesslog", middleware.StoreOS(middleware.AccessLogger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("accesslog is written")
 	}))))
 
-    mux.Handle("/userauth",middleware.Basicauth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	mux.Handle("/userauth", middleware.Basicauth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/text")
 		w.Write([]byte("basicauth is permissioned"))
-    }),"ユーザー名とパスワードを入力してください。"))
+	}), "ユーザー名とパスワードを入力してください。"))
+
+	mux.Handle("/gracefulshutdown", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 5)
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/text")
+		w.Write([]byte("graceful shutdown"))
+	}))
 
 	return mux
 
 }
-
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+
+	"sync"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -51,7 +55,36 @@ func realMain() error {
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
 
-	// TODO: サーバーをlistenする
-	http.ListenAndServe(port, mux)
+	var wg sync.WaitGroup
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			log.Print("error graceful shutdown", err)
+		} else {
+			log.Print("graceful shutdown")
+		}
+	}()
+
+	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	wg.Wait()
+
 	return nil
 }
